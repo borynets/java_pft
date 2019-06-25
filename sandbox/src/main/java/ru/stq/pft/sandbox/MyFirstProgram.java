4a4854b38c322e6b3a788de2d7d03a248373aae7
Nina <45877472+borynets@users.noreply.github.com>
2019-06-25 13:53:34 +0300

    Добавлено несколько функций, а также исправены некоторые ошибки

---
+++ sandbox/src/main/java/ru/stq/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,34 @@
+package ru.stq.pft.sandbox;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        hello("Nina");
+
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной" + l + "=" + area(l));
+
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника  со сторонами" + a + "и" + b + "=" + area(a, b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello," + somebody + "!");
+    }
+
+    public static double area(double len) {
+        return len * len;
+
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
+}
+
+
+
