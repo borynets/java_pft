e1f5342ed3c2902f4a2e2660353a81f026ea801a
Nina <45877472+borynets@users.noreply.github.com>
2019-06-26 18:17:41 +0300

    изменения в классе

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -3,11 +3,12 @@ package ru.stqa.pft.sandbox;
 public class Square {
 
     public double l;
-    public Square(double l){
-       this.l= l;
+
+    public Square(double l) {
+        this.l = l;
     }
 
-    public  double area() {
+    public double area() {
         return this.l * this.l;
     }
 }
